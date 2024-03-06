c11bd30af9fb43d9b1e3cba8ab5bd61fe4f36ac7
Vipul Dhurve <NH2868@nslhub.in>
2024-03-06 15:34:10 +0530

    Pushing Merge Sort Code

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -2,59 +2,55 @@ package sorting;
 
 public class MergeSort {
 
-    public void merge(int[] A, int start, int mid, int end){
+    public static void conquer(int[] arr, int start, int mid, int end){
+        if(start>end) return;
 
-            int m = mid - start + 1;
-            int n = end - mid;
+        int merged[] = new int[end-start+1];
+        int idx1=start;
+        int idx2=mid+1;
+        int x=0;
 
-        int[] a = new int[m];
-        int[] b = new int[n];
-
-        for(int i=start;i<=mid;i++){
-            a[i-start] = A[i];
-        }
-        for(int i=mid+1;i<=end;i++){
-            b[i-mid-1] = A[i];
-        }
-
-        int k=start, i=0, j=0;
-        while(i<m && j<n){
-            if(a[i] <= b[j]){
-                A[k++] = a[i++];
+        while(idx1<=mid && idx2<=end){
+            if(arr[idx1]<arr[idx2]){
+                merged[x++]=arr[idx1++];
             }else{
-                A[k++] = b[j++];
+                merged[x++]=arr[idx2++];
             }
         }
-        while(i<m){
-            A[k++] = a[i++];
+
+        while(idx1<=mid){
+            merged[x++]=arr[idx1++];
         }
-        while(j<n){
-            A[k++] = b[j++];
+
+        while(idx2<=end){
+            merged[x++]=arr[idx2++];
+        }
+
+        for(int i=0, j=start; i<merged.length; i++, j++){
+            arr[j]=merged[i];
         }
     }
 
-    public void sort(int[] a, int start, int end){
-        int n = end-start+1;
-        if(n==1) return;
+    public static void sort(int[] arr, int start, int end){
+        if(start>=end) return;
 
-        int mid = start + (end - start)/2;
-
-        sort(a, start, mid);
-        sort(a, mid+1, end);
-        merge(a, start, mid, end);
+        //divide
+        int mid = start+(end-start)/2;
+        sort(arr, start, mid);
+        sort(arr, mid+1, end);
+        //conquer
+        conquer(arr, start, mid, end);
     }
 
 
     public static void main(String[] args) {
 
-        MergeSort mergeSort = new MergeSort();
-
         int[] a = new int[]{1,3,5,7,9};
         int[] b = new int[]{2,4,6,8};
 
-        int[] unsorted = new int[]{2,1,4,3,6,5,8,7};
+        int[] unsorted = new int[]{2,1,4,3,6,5,8,7,9};
 
-        mergeSort.sort(unsorted, 0, unsorted.length-1);
+        sort(unsorted, 0, unsorted.length-1);
         printArray(unsorted);
     }
 
