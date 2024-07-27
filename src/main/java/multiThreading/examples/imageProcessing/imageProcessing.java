d8768789a4f363efc748b1ecafcfcc088903c9e6
vipuldhurve <vipuldhurve@gmail.com>
2024-07-27 20:16:37 +0530

    Added image processing example int multiThreading

---
+++ src/main/java/multiThreading/examples/imageProcessing/imageProcessing.java
@@ -0,0 +1,92 @@
+package multiThreading.examples.imageProcessing;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class imageProcessing {
+    public static final String SOURCE_FILE = "src/main/java/multiThreading/examples/imageProcessing/many-flowers.jpg";
+    public static final String DESTINATION_FILE = "src/main/java/multiThreading/examples/imageProcessing/out/many-flowers.jpg";
+
+    public static void main(String[] args) throws IOException {
+        BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+        BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        System.out.println("Recoloring image: white to purple flowers");
+        recolorSingleThreaded(originalImage, resultImage);
+
+        File outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+
+//        Open the result image with the default image viewer
+//        if (Desktop.isDesktopSupported()) {
+//            Desktop desktop = Desktop.getDesktop();
+//            if (desktop.isSupported(Desktop.Action.OPEN)) {
+//                desktop.open(outputFile);
+//            }
+//        }
+    }
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner, int width, int height) {
+        for (int x = leftCorner; x < leftCorner + width && x < originalImage.getWidth(); x++) {
+            for (int y = topCorner; y < topCorner + height && y < originalImage.getHeight(); y++) {
+                recolorPixel(originalImage, resultImage, x, y);
+            }
+        }
+    }
+
+    public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
+        int rgb = originalImage.getRGB(x, y);
+
+//      get rgb components from rgb of original image
+        int red = getRed(rgb), green = getGreen(rgb), blue = getBlue(rgb);
+
+        int newRed = red, newGreen = green, newBlue = blue;
+//      check to change pixel color
+        if (isShadeOfGrey(red, green, blue)) {
+            newRed = Math.min(255, red + 10);
+            newGreen = Math.max(0, green - 80);
+            newBlue = Math.max(0, blue - 20);
+        }
+//      create new rgb
+        int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+//      set pixel color in result image
+        setRGB(resultImage, x, y, newRGB);
+    }
+
+    public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+        image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+    }
+
+    public static boolean isShadeOfGrey(int red, int green, int blue) {
+        return Math.abs(red - green) < 30 && Math.abs(green - blue) < 30 && Math.abs(blue - red) < 30;
+    }
+
+    public static int createRGBFromColors(int red, int green, int blue) {
+        int rgb = 0;
+        rgb |= blue;
+        rgb |= green << 8;
+        rgb |= red << 16;
+        // Setting alpha(transparency)
+        rgb |= 0xFF000000;
+        return rgb;
+    }
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getBlue(int rgb) {
+        return (rgb & 0x000000FF);
+    }
+}
