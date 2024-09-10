16b51c7806c0e46c6638e404bd5206445d881a95
vipuldhurve <vipuldhurve@gmail.com>
2024-09-10 13:43:31 +0530

    Added backtracking dsa problems

---
+++ src/main/java/dsa/backtracking/CombinationSum2.java
@@ -0,0 +1,78 @@
+package dsa.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum2 {
+//    You are given an array of integers nums, which may contain duplicates,
+//    and a target integer target.
+//    Your task is to return a list of all unique combinations of nums
+//    where the chosen numbers sum to target.
+
+//    Each element from nums may be chosen at most once within a combination.
+//    The solution set must not contain duplicate combinations.
+
+//    You may return the combinations in any order
+//    and the order of the numbers in each combination can be in any order.
+
+//    Example 1:
+//    Input: candidates = [9,2,2,4,6,1,5], target = 8
+//    Output: [
+//            [1,2,5],
+//            [2,2,4],
+//            [2,6]
+//            ]
+
+//    Example 2:
+//    Input: candidates = [1,2,3,4,5], target = 7
+//    Output: [
+//            [1,2,4],
+//            [2,5],
+//            [3,4]
+//            ]
+
+    static List<List<Integer>> result;
+
+    private static void findCombinations(int[] candidates, int index, int target, List<Integer> sub) {
+        if (target == 0) {
+            result.add(new ArrayList<>(sub));
+            return;
+        }
+//         Since array is sorted we stop if nums[i] is greater than target
+        if (index >= candidates.length || candidates[index] > target) return;
+
+//        Include
+        sub.add(candidates[index]);
+        findCombinations(candidates, index + 1, target - candidates[index], sub);
+        sub.remove(sub.size() - 1);
+
+//        Not include and skip all same values
+        while (index + 1 < candidates.length && candidates[index] == candidates[index + 1]) index++;
+        findCombinations(candidates, index + 1, target, sub);
+    }
+
+    private static void solve(int[] candidates, int target) {
+//        Sort input array
+        Arrays.sort(candidates);
+        System.out.println("Input: " + Arrays.toString(candidates) + " Target = " + target);
+        result = new ArrayList<>();
+        findCombinations(candidates, 0, target, new ArrayList<>());
+        System.out.println("Output: " + result.toString() + "\n");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{9, 2, 2, 4, 6, 1, 5};
+        int target = 8;
+        solve(input, target);
+
+        input = new int[]{1, 2, 2, 2, 3};
+        target = 5;
+        solve(input, target);
+
+        input = new int[]{1, 2, 3, 4, 5};
+        target = 7;
+        solve(input, target);
+    }
+}
---
+++ src/main/java/dsa/backtracking/Subsets.java
@@ -0,0 +1,51 @@
+package dsa.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Subsets {
+//    Given an integer array nums of unique elements, return all possible
+//    subsets(the power set).
+
+//    The solution set must not contain duplicate subsets. Return the solution in any order.
+
+//    Example 1:
+//    Input: nums = [1,2,3]
+//    Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+//    Example 2:
+//    Input: nums = [0]
+//    Output: [[],[0]]
+
+    static List<List<Integer>> powerSet;
+
+    private static void findSubsets(int[] nums, int i, List<Integer> sub) {
+        if (i >= nums.length) {
+            powerSet.add(new ArrayList<>(sub));
+            return;
+        }
+//        include i element
+        sub.add(nums[i]);
+        findSubsets(nums, i + 1, sub);
+        sub.remove(sub.size() - 1);
+//        not include i element
+        findSubsets(nums, i + 1, sub);
+    }
+
+    public static void main(String[] args) {
+        int[][] inputs = new int[][]{
+                {1, 2, 3},
+                {0}
+        };
+        Arrays.stream(inputs)
+                .peek(i -> System.out.println("Input: " + Arrays.toString(i)))
+                .forEach(i -> {
+                    powerSet = new ArrayList<>();
+                    findSubsets(i, 0, new ArrayList<>());
+                    System.out.println("Output: " + powerSet.toString() + "\n");
+                });
+    }
+}
+
