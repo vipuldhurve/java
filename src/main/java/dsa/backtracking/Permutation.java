e961b1e751d580ec673f8896bd05c3ec58d509d9
vipuldhurve <vipuldhurve@gmail.com>
2024-09-12 23:48:27 +0530

    Added permutations in backtracking dsa

--- src/main/java/dsa/backtracking/Permutation.java
+++ src/main/java/dsa/backtracking/Permutation.java
@@ -22,7 +22,6 @@ public class Permutation {
     private static List<List<Integer>> permute(int[] nums) {
 //        List to store permutations of array
         List<List<Integer>> result = new ArrayList<>();
-
 //        If only 1 element
         if (nums.length == 1) {
             List<Integer> singleNum = new ArrayList<>();
@@ -30,23 +29,18 @@ public class Permutation {
             result.add(singleNum);
             return result;
         }
-
 //        Find permutation of array if size > 1
         for (int i = 0; i < nums.length; i++) {
             int currentNum = nums[i];
-
 //            Fix one element
             int[] remainingNums = new int[nums.length - 1];
             int idx = 0;
-
 //            Create array of remaining elements
             for (int j = 0; j < nums.length; j++) {
                 if (j != i) remainingNums[idx++] = nums[j];
             }
-
 //            Recursively find permutations of remaining elements
             List<List<Integer>> perms = permute(remainingNums);
-
 //            Add currentNum to all permutations
             for (List<Integer> perm : perms) {
                 perm.add(currentNum);
@@ -78,18 +72,14 @@ public class Permutation {
             result.add(new ArrayList<>(permutation));
             return;
         }
-
         for (int i = 0; i < nums.length; i++) {
 //            Skip used elements
             if (used[i]) continue;
-
 //            Add element in permutation and mark used
             permutation.add(nums[i]);
             used[i] = true;
-
 //            Recursively build rest of the permutation
             sortedPermuteBacktrack(nums, permutation, used, result);
-
 //            Backtrack: remove nums[i] and mark it ias unused
             permutation.remove(permutation.size() - 1);
             used[i] = false;
@@ -101,7 +91,6 @@ public class Permutation {
                 {1, 2, 3},
                 {7}
         };
-
         Arrays.stream(input)
                 .peek(i -> System.out.println("INPUT: " + Arrays.toString(i)))
                 .forEach(i -> {
